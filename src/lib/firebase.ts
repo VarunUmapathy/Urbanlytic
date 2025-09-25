de637bb8871327f0503d26c7ca3484085c1f3fa8
Varun Umapathy <varunu282004@gmail.com>
2025-09-25 19:14:04 +0530

    Fixed images

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAQM4NcOrgkrERq8yORoXvvrNmkcLA_t8g",
   authDomain: "oval-bot-472512-s4.firebaseapp.com",
   projectId: "oval-bot-472512-s4",
-  storageBucket: "oval-bot-472512-s4.appspot.com",
+  storageBucket: "oval-bot-472512-s4.firebasestorage.app",
   messagingSenderId: "883976203495",
   appId: "1:883976203495:web:d1df31aeae53e4c00f8642",
   measurementId: "G-MYL5XLGRQL"
